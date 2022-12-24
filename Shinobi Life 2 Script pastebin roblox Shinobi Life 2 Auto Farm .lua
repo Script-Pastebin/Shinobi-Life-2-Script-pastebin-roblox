1e76b94cd0fed0d18e5fbaffb28856e95bb73478
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-24 20:51:52 +0100

    commit zero

---
+++ Shinobi Life 2 Script pastebin roblox Shinobi Life 2 Auto Farm .lua
@@ -0,0 +1,11 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+loadstring(game:HttpGet("https://pastebin.com/raw/6Z08vuiu", true))()
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							Really awesome GUI here with lots of features. Works on all free on Exploits
+						
+]]
